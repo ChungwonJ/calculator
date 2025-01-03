6b44d20e7826fa1d997f8d40e14ecafacc3baac5
chungwon <wjdcjddnjs1234@gmail.com>
2025-01-03 19:39:52 +0900

    calculator1

--- CalculatorProject/src/main/java/com/example/calculator1/Main.java
+++ CalculatorProject/src/main/java/com/example/calculator1/Main.java
@@ -1,17 +1,37 @@
 package com.example.calculator1;
 
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
         int firstNumber = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNumber = sc.nextInt();
 
+        sc.nextLine();
 
-        System.out.print("첫 번째 숫자를 입력하세요: " + firstNumber);
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요: " + secondNumber);
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String type = sc.nextLine();
+
+        int result = 0;
+
+        if(type.equals("+")){
+            result = firstNumber + secondNumber;
+        } else if (type.equals("-")){
+            result = firstNumber - secondNumber;
+        } else if (type.equals("*")){
+            result = firstNumber * secondNumber;
+        } else if (type.equals("/")) {
+            result = firstNumber / secondNumber;
+        } else {
+            return;
+        }
+        System.out.println("결과: " + result);
     }
 }
