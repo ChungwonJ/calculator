18f8805e5223b850b703d253a986cf06de5c20d9
chungwon <wjdcjddnjs1234@gmail.com>
2025-01-03 21:18:22 +0900

    finish

--- CalculatorProject/src/main/java/com/example/calculator1/Main.java
+++ CalculatorProject/src/main/java/com/example/calculator1/Main.java
@@ -7,32 +7,43 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Boolean finish = true;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = sc.nextInt();
+        while (finish) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNumber = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = sc.nextInt();
+            sc.nextLine();
 
-        sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String type = sc.nextLine();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String type = sc.nextLine();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNumber = sc.nextInt();
 
-        int result = 0;
+            int result = 0;
 
-        if(type.equals("+")){
-            result = firstNumber + secondNumber;
-        } else if (type.equals("-")){
-            result = firstNumber - secondNumber;
-        } else if (type.equals("*")){
-            result = firstNumber * secondNumber;
-        } else if (type.equals("/")) {
-            result = firstNumber / secondNumber;
-        } else {
-            System.out.println("잘못되었습니다");
-            return;
+            if (type.equals("+")) {
+                result = firstNumber + secondNumber;
+            } else if (type.equals("-")) {
+                result = firstNumber - secondNumber;
+            } else if (type.equals("*")) {
+                result = firstNumber * secondNumber;
+            } else if (type.equals("/")) {
+                result = firstNumber / secondNumber;
+            } else {
+                System.out.print("잘못되었습니다");
+                return;
+            }
+            System.out.println("결과: " + result);
+
+            sc.nextLine();
+            System.out.print("더 고고?: ");
+            String go = sc.nextLine();
+            if (go.equals("exid")) {
+                System.out.print("종료되었습니다");
+                break;
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
